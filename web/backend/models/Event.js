f429f1aa3914d1dbd77c31de32c343e577e19f1a
Yasmin-Bushra <yasminbushraa@gmail.com>
2025-09-16 01:37:31 +0400

    refactor: replace Event* files with Conference* implementation

--- web/backend/models/Event.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const eventSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  date: String,
-  description: String,
-});
-
-export default mongoose.model("Event", eventSchema);
